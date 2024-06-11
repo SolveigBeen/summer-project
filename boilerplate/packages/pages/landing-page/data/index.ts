59a8339984c1273384c21f30dadb40fa5ad093f5
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-06-11 11:04:25 +0200

    Added code

--- boilerplate/packages/pages/landing-page/data/index.ts
+++ boilerplate/packages/pages/landing-page/data/index.ts
@@ -1,15 +1,15 @@
-export type ChuckNorrisResponse = {
+export interface ChuckNorrisResponse {
   icon_url: string;
   id: string;
   url: string;
   value: string;
-};
+}
 
 export const useData = () => {
   return {
     async fetchQuote(): Promise<ChuckNorrisResponse | void> {
       try {
-        const URL = "https://api.chucknorris.io/jokes/random";
+        const URL = 'https://api.chucknorris.io/jokes/random';
         const resp = await fetch(URL);
         return await resp.json();
       } catch (error) {
